96e6c4616bfde9ec76558fb9174b4a6e908ac4e6
unknown <carl.castello96@gmail.com>
2020-05-01 13:33:03 -0600

    FC-1: Set up material-UI and pre-organized the quiz folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component, ReactNode } from 'react';
+
+import Quiz from './quiz';
+
+class Page extends Component {
+  render(): ReactNode {
+    return(
+      <Quiz/>
+    );
+  }
+}
+
+export default Page;
---
+++ src/pages/quiz/index.tsx
@@ -0,0 +1,13 @@
+import React, { Component, ReactNode } from 'react';
+
+class Quiz extends Component {
+  render(): ReactNode {
+    return(
+      <div>
+        Hello
+      </div>            
+    );
+  }
+}
+
+export default Quiz;
