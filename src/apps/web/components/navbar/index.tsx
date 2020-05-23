d527ec6464e86488dbc2dea94a71f06ef4428cd5
Carl Castello <carl.castello@yahoo.com>
2020-05-22 21:54:04 -0600

    FC-12: styled navbar

--- src/apps/web/components/navbar/index.tsx
+++ src/apps/web/components/navbar/index.tsx
@@ -3,18 +3,24 @@ import { Link } from 'react-router-dom';
 
 import { withStyles, AppBar, Toolbar, Typography } from '@material-ui/core';
 
-import { IOwnProps, IOwnState } from './types';
+import { IOwnProps } from './types';
 
 import styles from './styles';
 
 
-class Navbar extends Component<IOwnProps, IOwnState> {
+class Navbar extends Component<IOwnProps> {
   render(): ReactNode {
+    const {
+      classes: {
+        link,
+      }
+    } = this.props;
+
     return (
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h5">
-            <Link to=''>
+            <Link to='' className={link}>
               Flashcard
             </Link>
           </Typography>
